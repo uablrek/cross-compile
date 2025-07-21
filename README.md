6eeb652d8c57660121f89133b34e044be2d78b52
Lars Ekman <uablrek@gmail.com>
2025-07-21 09:31:33 +0200

    Add content

--- README.md
+++ README.md
@@ -1,2 +1,186 @@
-# cross-compile
-Howto cross-compile on Linux for arm64 (aarch64)
+# Cross Compile
+
+Cross compile is often non-trivial, but not so hard as you might
+think. This repo describes how to cross compile to the ARM64
+architecture (aarch64), for example to [RPi 4](
+https://www.raspberrypi.com/products/raspberry-pi-4-model-b/) and
+[Radxa Rock 4se](https://wiki.radxa.com/Rock4/se) SoC boards.  I
+rarely use official distributions, so native build is not an option,
+besides it's *unbearably slow*.
+
+I use Ubuntu Linux (on an x86_64 pc), so some ways may not work, or
+work differently, on other distros. But locally built tools like,
+[musl-cross-make](https://github.com/richfelker/musl-cross-make),
+should work.
+
+For now I use `gcc`, but I will probably look at `clang` in the
+future. I like the [clang cross compilation](
+https://clang.llvm.org/docs/CrossCompilation.html) better, just use
+another LLVM backend!
+
+An advantage with cross compilation is that you can test your progams in
+virtual environment (qemu-system-aarch64).
+
+A *major* problem with cross compilation is library dependencies.
+For native builds the libraries are installed, but not so when you
+cross compile. Basically you have 2 options: extract the needed libs
+from your target, or build them yourself. As I usually don't use a
+target distribution, I take the second option.
+
+Most things are done with the `admin.sh` script.
+
+```
+./admin.sh                    # help printout
+./admin.sh env                # current settings
+./admin.sh versions           # Used versions and download/clone status
+```
+
+Everything is built in `$XCOMPILE_WORKSPACE`, which defaults to
+"/tmp/tmp/$USER/xcompile". Default for options can be set as
+environment variables. Example:
+
+```
+export __arch=aarch64
+```
+
+Quick cross compile example:
+```
+# Cross compile a static BusyBox for aarch64
+sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+ver_busybox=busybox-1.36.1
+curl --output-dir $HOME/Downloads -O -L https://busybox.net/downloads/$ver_busybox.tar.bz2
+ws=/tmp/tmp/$USER/xcompile-test
+mkdir -p $ws
+tar -C $ws -xf $HOME/Downloads/$ver_busybox.tar.bz2
+cp config/$ver_busybox $ws/$ver_busybox/.config
+cd $ws/$ver_busybox
+make menuconfig
+# Set Settings>Cross compiler prefix to "aarch64-linux-gnu-" (tailing dash included)
+make -j$(nproc)
+file busybox   # Should be executable, ARM aarch64
+#./admin.sh busybox_build --arch=aarch64   # Does the same thing
+```
+
+## Linux kernel
+
+The Linux kernel is simple to cross compile. Always use the `O=`
+option to kernel `make` to keep source and objects separated. This
+allows the same source tree to be used for different builds.
+
+```
+#sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+ver_kernel=linux-6.15.7
+curl --output-dir $HOME/Downloads -O https://cdn.kernel.org/pub/linux/kernel/v6.x/$ver_kernel.tar.xz
+KERNELDIR=$HOME/tmp/linux
+mkdir -p $KERNELDIR
+tar -C $KERNELDIR -xf $HOME/Downloads/$ver_kernel.tar.xz
+cd $KERNELDIR/$ver_kernel
+ws=/tmp/tmp/$USER/xcompile-test/aarch64
+__kobj=$ws/obj/$ver_kernel
+make  O=$__kobj ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- virt.config
+make O=$__kobj ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+```
+
+Or
+```
+export __arch=aarch64
+export __kcfg=/tmp/linux.config
+./admin.sh kernel_build --initconfig=virt.config
+```
+
+## Test with qemu
+
+With a kernel and a static busybox we can test to cross compile a
+program and run it in `qemu`. First, the program:
+
+```
+cat > /tmp/hello.c <<EOF
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+EOF
+mkdir -p /tmp/root-aarch64
+aarch64-linux-gnu-gcc -static -o /tmp/root-aarch64/hello /tmp/hello.c
+```
+
+Now build the kernel and `BosyBox` and test it:
+```
+export __arch=aarch64
+unset __kcfg      # (if you have set it above)
+./admin.sh setup --clean
+./admin.sh qemu --root=/tmp/root-aarch64
+# In qemu
+uname -a
+./hello
+<ctrl-c>      # to exit
+```
+
+## Dynamic linking
+
+So far we have used statically linked programs. For dynamically liked
+programs we need a `loader` and the libraries. Build with:
+
+```
+aarch64-linux-gnu-gcc -o /tmp/root-aarch64/hello /tmp/hello.c
+file /tmp/root-aarch64/hello
+```
+
+If you try to run again i `qemu`, you will get a `./hello: not found`.
+This is confusing, because `./hello` *is* there. You can see it with
+`ls`! But what's not found is the loader: `/lib/ld-linux-aarch64.so.1`.
+So, let's add it and try again:
+
+```
+mkdir -p /tmp/root-aarch64/lib
+cp /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 /tmp/root-aarch64/lib
+```
+This time you get:
+```
+./hello: error while loading shared libraries: libc.so.6: cannot open shared object file: No such file or directory
+```
+which is an improvement. Add the lib and try again:
+```
+cp /usr/aarch64-linux-gnu/lib/libc.so.6 /tmp/root-aarch64/lib
+```
+Success!
+
+### ldd
+
+The `ldd` program lists the dynamic library dependencies for a
+program. You don't have `ldd` in `qemu` but you can do what `ldd` does
+(it's likely a script on your pc):
+
+```
+# In qemu
+~ # LD_TRACE_LOADED_OBJECTS=1 ./hello 
+        linux-vdso.so.1 (0x0000ffff940ed000)
+        libc.so.6 => /lib/libc.so.6 (0x0000ffff93ef0000)
+        /lib/ld-linux-aarch64.so.1 (0x0000ffff940b0000)
+```
+
+You can use a similar command on your pc. Please check `man ld-linux.so`
+for more info.
+
+## Musl libc
+
+[Musl libc](https://musl.libc.org/) is an alternative to GNU libc. It
+produces smaller binaries, and has a more relaxed license. It is used
+for instance by [Alpine Linux](https://www.alpinelinux.org/).
+
+On Ubuntu `musl` is available for native builds:
+```
+sudo apt install musl-dev:amd64
+gcc /tmp/hello.c -static -o /tmp/hello-gnu
+x86_64-linux-musl-gcc /tmp/hello.c -static -o /tmp/hello-musl
+ll /tmp/hello-*
+-rwxrwxr-x 1 uablrek uablrek 767K Jul 21 09:23 /tmp/hello-gnu*
+-rwxrwxr-x 1 uablrek uablrek  25K Jul 21 09:23 /tmp/hello-musl*
+```
+
+But we want to cross compile, and the recommended way is to use
+[musl-cross-make](https://github.com/richfelker/musl-cross-make).
+
+To be continued...
---
+++ admin.sh
@@ -0,0 +1,437 @@
+#! /bin/sh
+##
+## xcompile/admin.sh --
+##
+##   Admin script for cross-compile
+##
+## Commands;
+##
+
+prg=$(basename $0)
+dir=$(dirname $0); dir=$(readlink -f $dir)
+me=$dir/$prg
+test -n "$TEMP" || TEMP=/tmp/tmp/$USER
+tmp=$TEMP/${prg}_$$
+
+die() {
+    echo "ERROR: $*" >&2
+    rm -rf $tmp
+    exit 1
+}
+help() {
+    grep '^##' $0 | cut -c3-
+    rm -rf $tmp
+    exit 0
+}
+test -n "$1" || help
+echo "$1" | grep -qi "^help\|-h" && help
+
+log() {
+	echo "$*" >&2
+}
+findf() {
+	local d
+	for d in $(echo $FSEARCH_PATH | tr : ' '); do
+		f=$d/$1
+		test -r $f && return 0
+	done
+	unset f
+	return 1
+}
+findar() {
+	findf $1.tar.bz2 || findf $1.tar.gz || findf $1.tar.xz || findf $1.zip
+}
+# Set variables unless already defined. Vars are collected into $opts
+eset() {
+	local e k
+	for e in $@; do
+		k=$(echo $e | cut -d= -f1)
+		opts="$opts|$k"
+		test -n "$(eval echo \$$k)" || eval $e
+	done
+}
+# cdsrc <version>
+# Cd to the source directory. Unpack the archive if necessary.
+cdsrc() {
+	test -n "$1" || die "cdsrc: no version"
+	test "$__clean" = "yes" && rm -rf $WS/$1
+	if ! test -d $WS/$1; then
+		findar $1 || die "No archive for [$1]"
+		if echo $f | grep -qF '.zip'; then
+			unzip -d $WS -qq $f || die "Unzip [$f]"
+		else
+			tar -C $WS -xf $f || die "Unpack [$f]"
+		fi
+	fi
+	cmd_pkgconfig
+	cd $WS/$1
+}
+# Define sw versions
+# The pattern "name-ver" is de'facto standard
+versions() {
+	eset \
+		ver_kernel=linux-6.15.7 \
+		ver_busybox=busybox-1.36.1 \
+		ver_expat=expat-2.6.2 \
+		ver_musl=musl-cross-make-master \
+		ver_zlib=zlib-1.3.1 \
+		ver_libpciaccess=libpciaccess-libpciaccess-0.18.1 \
+		ver_pcre2=pcre2-10.44
+}
+##   env
+##     Print environment.
+cmd_env() {
+	test "$envset" = "yes" && return 0
+	envset=yes
+	versions
+	unset opts
+
+	eset ARCHIVE=$HOME/archive
+	eset FSEARCH_PATH=$HOME/Downloads:$ARCHIVE
+	eset XCOMPILE_WORKSPACE=/tmp/tmp/$USER/xcompile
+	eset __arch=x86_64
+	eset __native=no
+	test "$__native" = "yes" && __arch=$(uname -m)
+	WS=$XCOMPILE_WORKSPACE/$__arch
+	eset KERNELDIR=$HOME/tmp/linux
+	eset \
+		WS='' \
+		__musl=no \
+		__kcfg=$dir/config/$ver_kernel-$__arch \
+		__kdir=$KERNELDIR/$ver_kernel \
+		__kobj=$WS/obj/$ver_kernel \
+		__bbcfg=$dir/config/$ver_busybox \
+		__initrd=$WS/initrd.bz \
+		musldir=$HOME/tmp/musl-cross-make \
+		kernel=''
+	eset \
+		__sysd=$WS/sys
+
+	if test "$cmd" = "env"; then
+		set | grep -E "^($opts)="
+		exit 0
+	fi
+
+	mkdir -p $__sysd
+	test -n "$long_opts" && export $long_opts
+
+	if test "$__native" = "yes"; then
+		cc_setup=''
+		musl_setup=''
+		meson_setup=''
+	elif test "$__musl" = "yes"; then
+		test -x $musldir/$__arch/bin/$__arch-linux-musl-gcc || \
+			die "No musl cross-compiler built for [$__arch]"
+		export PATH=$musldir/$__arch/bin:$PATH
+		cc_setup="CC=$__arch-linux-gnu-gcc AR=$__arch-linux-gnu-ar"
+		musl_at="--host=$__arch-linux-gnu"
+		musl_meson="--cross-file $dir/config/meson-cross-musl.$__arch"
+	else
+		cc_setup="CC=$__arch-linux-gnu-gcc AR=$__arch-linux-gnu-ar"
+		musl_at="--host=$__arch-linux-gnu"
+		musl_meson="--cross-file $dir/config/meson-cross.$__arch"
+	fi
+	mkdir -p $WS || die "Can't mkdir [$WS]"
+	if test "$__arch" = "aarch64"; then
+		kernel=$__kobj/arch/arm64/boot/Image
+	else
+		kernel=$__kobj/arch/x86/boot/bzImage
+	fi
+	cd $dir
+}
+##   versions [--brief]
+##     Print used sw versions
+cmd_versions() {
+	unset opts
+	versions
+	set | grep -E "^($opts)="
+	test "$__brief" = "yes" && return 0
+	local k v
+	for k in $(echo $opts | tr '|' ' '); do
+		v=$(eval echo \$$k)
+		if findar $v; then
+			printf "%-20s (%s)\n" $v $f
+		else
+			printf "%-20s (archive missing!)\n" $v
+		fi
+	done
+}
+##   setup [--clean]
+##     Build everything
+cmd_setup() {
+	test "$__clean" = "yes" && rm -rf $WS
+	$me busybox_build || die busybox_build
+	$me kernel_build || die kernel_build
+}
+##   pkgconfig <cmd>
+##     Fixup pkgconfig files, and set $PKG_CONFIG_LIBDIR
+##     Used internally for build setup. Cli use:
+##     ./xcompile.sh pkgconfig pkg-config --libs --cflags <lib>
+cmd_pkgconfig() {
+	local d
+	mkdir -p $__sysd/pkgconfig-sys
+	unset PKG_CONFIG_PATH
+	export PKG_CONFIG_LIBDIR=$__sysd/pkgconfig-sys
+	for d in $(find $__sysd -type d -name pkgconfig); do
+		cp $d/* $PKG_CONFIG_LIBDIR
+	done
+	sed -i -e "s,prefix=/usr/local,prefix=$__sysd/usr/local," \
+		$__sysd/pkgconfig-sys/* > /dev/null 2>&1
+	if test "$cmd" = "pkgconfig"; then
+		test -n "$1" && $@
+	fi
+}
+##   strip <dir>
+##     Recursive architecture and lib sensitive strip
+cmd_strip() {
+	test -n "$1" || die "No dir"
+	test -d "$1" || die "Not a directory [$1]"
+	local strip=strip
+	test "$__musl" = "yes" && \
+		strip=$musldir/$__arch/bin/$__arch-linux-musl-strip
+	local f
+	cd $1
+	for f in $(find . -type f -executable); do
+		file $f | grep -q ELF && $strip $f
+	done
+}
+##   musl-cross-make-build
+cmd_musl_cross_make_build() {
+	if test -r $musldir/Makefile; then
+		cd $musldir
+	else
+		cdsrc $ver_musl
+	fi
+	pwd
+	local make="make -j$(nproc) GCC_VER=13.3.0"
+	local target arch
+	for arch in aarch64 x86_64; do
+		target=$arch-linux-musl
+		$make TARGET=$target || die "make $target"
+		$make TARGET=$target install OUTPUT=$PWD/$arch \
+			|| die "make $target install"
+	done
+}
+##   musl_install <dest>
+##     Install musl libs. This is a no-op if --musl is not specified
+cmd_musl_install() {
+	test "$__musl" = "yes" || return 0
+	test -n "$1" || die "No dest"
+	local libd=$musldir/$__arch/$__arch-linux-musl/lib
+	test -d $libd || die "Not a directory [$libd]"
+	mkdir -p "$1/lib" || die "Mkdir failed [$1/lib]"
+	cp $libd/libc.so $1/lib/ld-musl-$__arch.so.1  # The loader
+	cp -L $libd/lib*.so.[0-9] $1/lib
+}
+##   busybox_build [--bbcfg=] [--menuconfig]
+##     Build BusyBox
+cmd_busybox_build() {
+	cdsrc $ver_busybox
+	if test "$__menuconfig" = "yes"; then
+		test -r $__bbcfg && cp $__bbcfg .config
+		make menuconfig
+		cp .config $__bbcfg
+	else
+		test -r $__bbcfg || die "No config"
+		cp $__bbcfg .config
+	fi
+	test "$__native" != "yes" && \
+		sed -i -E "s,CONFIG_CROSS_COMPILER_PREFIX=\"\",CONFIG_CROSS_COMPILER_PREFIX=\"$__arch-linux-gnu-\"," .config
+	make -j$(nproc)
+}
+cmd_kernel_unpack() {
+	test -d $__kdir && return 0	  # (already unpacked)
+	log "Unpack kernel to [$__kdir]..."
+	findar $ver_kernel || die "Kernel source not found [$ver_kernel]"
+	mkdir -p $KERNELDIR
+	tar -C $KERNELDIR -xf $f
+}
+##   kernel_build --initconfig=something_default  # Init the kcfg
+##   kernel_build [--clean] [--menuconfig]
+##     Build the kernel
+cmd_kernel_build() {
+	cmd_kernel_unpack
+	test "$__clean" = "yes" && rm -rf $__kobj
+	mkdir -p $__kobj
+
+	local CROSS_COMPILE make targets
+	make="make -C $__kdir O=$__kobj"
+	if test "$__native" != "yes"; then
+		if test "$__arch" = "aarch64"; then
+			make="$make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-"
+			targets="Image modules dtbs"
+		else
+			make="$make ARCH=x86_64 CROSS_COMPILE=x86_64-linux-gnu-"
+		fi
+	fi
+	if test -n "$__initconfig"; then
+		rm -r $__kobj
+		mkdir -p $__kobj $(dirname $__kcfg)
+		$make -C $__kdir O=$__kobj $__initconfig
+		cp $__kobj/.config $__kcfg
+		__menuconfig=yes
+	fi
+
+	test -r $__kcfg || die "Not readable [$__kcfg]"
+	cp $__kcfg $__kobj/.config
+	if test "$__menuconfig" = "yes"; then
+		$make menuconfig
+		cp $__kobj/.config $__kcfg
+	else
+		$make oldconfig
+	fi
+	$make -j$(nproc) $targets
+}
+##   initrd_build [--initrd=] [--root=dir]
+##     Build a ramdisk (cpio archive) containing busybox and the --root
+cmd_initrd_build() {
+	local bb=$WS/$ver_busybox/busybox
+	test -x $bb || die "Not executable [$bb]"
+	touch $__initrd || die "Can't create [$__initrd]"
+
+	cmd_gen_init_cpio
+	gen_init_cpio=$WS/bin/gen_init_cpio
+	mkdir -p $tmp
+	cat > $tmp/cpio-list <<EOF
+dir /dev 755 0 0
+nod /dev/console 644 0 0 c 5 1
+dir /bin 755 0 0
+file /bin/busybox $bb 755 0 0
+slink /bin/sh busybox 755 0 0
+dir /etc 755 0 0
+file /init $dir/config/init-tiny 755 0 0
+EOF
+	if test -n "$__root"; then
+		test -d "$__root" || die "Not a directory [$__root]"
+		cmd_emit_list $__root >> $tmp/cpio-list
+	fi
+	rm -f $__initrd
+	local uncompressed=$(echo $__initrd | sed -E 's,.[a-z]+$,,')
+	local compression=$(echo $__initrd | grep -oE '[a-z]+$')
+	case $compression in
+		xz)
+			$gen_init_cpio $tmp/cpio-list > $uncompressed
+			xz -T0 $uncompressed;;
+		gz)
+			$gen_init_cpio $tmp/cpio-list | gzip -c > $__initrd;;
+		bz)
+			$gen_init_cpio $tmp/cpio-list | bzip2 -c > $__initrd;;
+		*)
+			die "Unknown initrd compression [$compression]";;
+	esac
+}
+#   gen_init_cpio
+#     Build the kernel gen_init_cpio utility
+cmd_gen_init_cpio() {
+	local x=$WS/bin/gen_init_cpio
+	test -x $x && return 0
+	cmd_kernel_unpack	
+	mkdir -p $(dirname $x)
+	local src=$__kdir/usr/gen_init_cpio.c
+	test -r $src || die "Not readable [$src]"
+	gcc -o $x $src
+}
+#   emit_list <src>
+#     Emit a gen_init_cpio list built from the passed <src> dir
+cmd_emit_list() {
+	test -n "$1" || die "No source"
+	local x p d=$1
+	test -d $d || die "Not a directory [$d]"
+	cd $d
+	for x in $(find . -mindepth 1 -type d | cut -c2-); do
+		p=$(stat --printf='%a' $d$x)
+		echo "dir $x $p 0 0"
+	done
+	for x in $(find . -mindepth 1 -type f | cut -c2-); do
+		p=$(stat --printf='%a' $d$x)
+		echo "file $x $d$x $p 0 0"
+	done
+}
+##   expat_build
+cmd_expat_build() {
+	# Normal autotools cross-compile
+	cdsrc $ver_expat
+	test -r Makefile || ./configure $musl_at --without-docbook \
+		--without-tests --without-examples || die "configure"
+	make -j$(nproc) || die make
+	make install DESTDIR=$__sysd || die "make install"
+}
+##   zlib_build
+cmd_zlib_build() {
+	# Simplified autotools cross-compile
+	cdsrc $ver_zlib
+	env $musl_cc ./configure
+	make -j$(nproc) $musl_cc || die make
+	make install prefix=$__sysd/usr/local
+}
+##   libpciaccess_build
+cmd_libpciaccess_build() {
+	# Meson cross-compile
+	cdsrc $ver_libpciaccess
+	test -d build || meson setup $musl_meson -Dzlib=enabled build
+	meson compile -C build || die build
+	meson install -C build --destdir $__sysd
+}
+##   pcre2_build
+cmd_pcre2_build() {
+	# Cmake cross-compile. Use "cmake -LAH" to check options
+	cdsrc $ver_pcre2
+	local opt="-DBUILD_SHARED_LIBS=ON"
+	mkdir -p build
+	cd build
+	test -r "Makefile" || env $musl_cc cmake $opt .. || die cmake
+	make -j$(nproc) || die make
+	make install -j$(nproc) DESTDIR=$__sysd || die "make install"
+}
+
+##   qemu [--root=dir]
+##     Start a qemu VM. Optionally with files from --root
+cmd_qemu() {
+	test -r $kernel || die "Not readable [$kernel]"
+	cmd_initrd_build
+	rm -rf $tmp					# (since we 'exec')
+	qemu_$__arch $@
+}
+qemu_x86_64() {
+	exec qemu-system-x86_64 -enable-kvm -M q35 -m 128M -smp 4 \
+		-drive file=fat:rw:$__root,format=raw,media=disk \
+		-monitor none -serial stdio -kernel $kernel $@
+}
+qemu_aarch64() {
+	exec qemu-system-aarch64 -nographic -cpu cortex-a72 \
+		-machine virt,virtualization=on,secure=off \
+		-append "init=/init" \
+		-monitor none -serial stdio -kernel $kernel -initrd $__initrd $@
+
+#		-drive if=none,file=fat:rw:$__root,format=raw,media=disk,id=hd \
+#		-device virtio-blk-pci,drive=hd \
+#		-hda fat:rw:$__root \
+}
+
+##
+# Get the command
+cmd=$(echo $1 | tr -- - _)
+shift
+grep -q "^cmd_$cmd()" $0 $hook || die "Invalid command [$cmd]"
+
+while echo "$1" | grep -q '^--'; do
+	if echo $1 | grep -q =; then
+		o=$(echo "$1" | cut -d= -f1 | sed -e 's,-,_,g')
+		v=$(echo "$1" | cut -d= -f2-)
+		eval "$o=\"$v\""
+	else
+		o=$(echo "$1" | sed -e 's,-,_,g')
+		eval "$o=yes"
+	fi
+	long_opts="$long_opts $o"
+	shift
+done
+unset o v
+
+# Execute command
+trap "die Interrupted" INT TERM
+cmd_env
+cmd_$cmd "$@"
+status=$?
+rm -rf $tmp
+exit $status
